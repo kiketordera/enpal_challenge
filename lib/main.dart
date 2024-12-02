06ead6ce41991114f61d349fe5a72f37a80e8c74
Kike Tordera <dojranovski@gmail.com>
2024-12-02 13:14:04 +0100

    feat(ui): graph representing the information

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,33 @@
 import 'package:enpal_challenge/domain/data/model/monitoring_data.dart';
 import 'package:enpal_challenge/domain/data/repositories/monitoring_repository_impl.dart';
 import 'package:enpal_challenge/domain/repositories/monitoring_repository.dart';
+import 'package:enpal_challenge/presentation/pages/chart_page.dart';
+import 'package:enpal_challenge/presentation/theme/colors.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ForTesting());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class ForTesting extends StatelessWidget {
+  const ForTesting({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Scaffold(body: Center(child: DatePicker())));
+  }
+}
+
+class DataPoints extends StatelessWidget {
+  const DataPoints({super.key, required this.date});
+
+  final String date;
+
+  String _formatTime(DateTime timestamp) {
+    return DateFormat.Hm().format(timestamp);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,8 +40,7 @@ class MainApp extends StatelessWidget {
           title: const Text('Monitoring Data'),
         ),
         body: FutureBuilder<List<MonitoringData>>(
-          future: repository.fetchMonitoringData(
-              '2024-12-01', MonitoringType.solar),
+          future: repository.fetchMonitoringData(date, MonitoringType.solar),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
               return const Center(child: CircularProgressIndicator());
@@ -32,15 +50,53 @@ class MainApp extends StatelessWidget {
               return const Center(child: Text('No data available'));
             } else {
               final data = snapshot.data!;
-              return ListView.builder(
-                itemCount: data.length,
-                itemBuilder: (context, index) {
-                  final item = data[index];
-                  return ListTile(
-                    title: Text('Value: ${item.value.toString()}'),
-                    subtitle: Text('Timestamp: ${item.timestamp}'),
-                  );
-                },
+              final double chartWidth = data.length *
+                  50.0; // We leave 50px space between each data point on the x-axis for better readability
+              return SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: SizedBox(
+                  width: chartWidth < MediaQuery.of(context).size.width
+                      ? MediaQuery.of(context).size.width
+                      : chartWidth,
+                  child: LineChart(
+                    LineChartData(
+                      minX: 0,
+                      maxX: (data.length - 1).toDouble(),
+                      minY: 0,
+                      maxY: 10000,
+                      titlesData: FlTitlesData(
+                        bottomTitles: AxisTitles(
+                          sideTitles: SideTitles(
+                            showTitles: true,
+                            getTitlesWidget: (value, meta) {
+                              return Text(
+                                _formatTime(data[value.toInt()].timestamp),
+                              );
+                            },
+                          ),
+                        ),
+                        topTitles: const AxisTitles(
+                            sideTitles: SideTitles(showTitles: false)),
+                        rightTitles: const AxisTitles(
+                            sideTitles: SideTitles(showTitles: true)),
+                      ),
+                      borderData: FlBorderData(show: true),
+                      lineBarsData: [
+                        LineChartBarData(
+                          spots: data
+                              .asMap()
+                              .entries
+                              .map((e) => FlSpot(
+                                  e.key.toDouble(), e.value.value.toDouble()))
+                              .toList(),
+                          color: AppColors.darkBlue,
+                          isCurved: true,
+                          dotData: const FlDotData(show: false),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               );
             }
           },
---
+++ lib/presentation/pages/chart_page.dart
@@ -0,0 +1,67 @@
+import 'package:enpal_challenge/main.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart'; // Import intl package
+
+class DatePicker extends StatefulWidget {
+  const DatePicker({super.key});
+
+  @override
+  State<DatePicker> createState() => _DatePickerState();
+}
+
+class _DatePickerState extends State<DatePicker> {
+  DateTime? _selectedDate;
+
+  // Create a DateFormat instance
+  final DateFormat _dateFormatter = DateFormat('yyyy-MM-dd');
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? selected = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2050),
+    );
+
+    if (selected != null && selected != _selectedDate) {
+      setState(() {
+        _selectedDate = selected;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Format the selected date
+    String formattedDate = _selectedDate != null
+        ? _dateFormatter.format(_selectedDate!)
+        : 'No date selected';
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () => _selectDate(context),
+            child: const Text('Select Date'),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            'Selected Date: $formattedDate',
+            style: const TextStyle(fontSize: 18),
+          ),
+          Visibility(
+            visible: _selectedDate != null,
+            child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return DataPoints(date: formattedDate);
+                  }));
+                },
+                child: const Text('Show Data')),
+          )
+        ],
+      ),
+    );
+  }
+}
